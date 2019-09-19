391bb59f43ddd33881a7617d552698c060a2bf94
afra666 <48674984+afra666@users.noreply.github.com>
2019-09-19 10:58:51 +0800

    Update and rename 001.Main.java to 001.MyApp.java

--- 001.MyApp.java
+++ 001.MyApp.java
@@ -1,34 +1,34 @@
 package p;
-
-import org.apache.catalina.Context;
-import org.apache.catalina.LifecycleException;
+import  org.apache.catalina.*;
 import org.apache.catalina.startup.Tomcat;
 import org.apache.catalina.webresources.DirResourceSet;
 import org.apache.catalina.webresources.StandardRoot;
 
 import java.io.File;
 
-public class Main {
+public class MyApp {
     public static void main(String[] args) {
         Tomcat tomcat=new Tomcat();
-        String tmpdir=System.getProperty("java.io.tmpdir");
-        tomcat.setBaseDir(tmpdir);
+        tomcat.setBaseDir("C:\\Users\\ADMINI~1\\AppData\\Local\\Temp\\");
         tomcat.setPort(8080);
         Context context=tomcat.addWebapp("/abc",new File("web/").getAbsolutePath());
-        StandardRoot resources=new StandardRoot(context);
-        resources.addPreResources(new DirResourceSet(
-                resources,"D:/WEB-INF/classes",
-                new File(".").getAbsolutePath(),
-                "/"
-        ));
-        context.setResources(resources);
+        tomcat.getConnector();
+        /**以下设置Servlet调用**/
+        StandardRoot resource=new StandardRoot(context);
+        resource.addPreResources(
+                new DirResourceSet(
+                        resource,"/WEB-INF/classes",
+                        new File(".").getAbsolutePath(),
+                        "/"
+                ));
+        /****/
+        context.setResources(resource);
         tomcat.getConnector();
         try {
             tomcat.start();
         } catch (LifecycleException e) {
             e.printStackTrace();
         }
-
         tomcat.getServer().await();
     }
 }
